3a03a3d818a11e280f6b3c0a4337e6c8bb05debd
herulume <herulume@protonmail.com>
2018-12-09 00:15:17 +0000

    Fix graddle

--- src/screens/LoggedIn.js
+++ src/screens/LoggedIn.js
@@ -1,40 +1,46 @@
 import React, { Component } from "react";
 import { Button, Text, View } from "react-native";
 import deviceStorage from "../services/deviceStorage";
+import Config from "react-native-config";
+import axios from "axios";
 
 export default class LoggedIn extends Component {
   constructor(props) {
     super(props);
     this.state = {
       badges: [],
-      loading: false
+      loading: false,
+      error: ""
     };
+    this.validateJson = this.validateJson.bind(this);
   }
 
-  getBadges() {
+  componentDidMount() {
     const URL = Config.API_URL + Config.API_BADGES;
     this.setState({ error: "", loading: true });
     const jwt = deviceStorage.loadJWT();
+
     axios
       .post(URL, {
         headers: {
-          Authorization: "Bearer " + jwt
+          Authorization: "Bearer: " + jwt
         }
       })
       .then(res => {
         this.validateJson(res);
       })
       .catch(() => {
-        this.setState({ loading: false, error: "Error loading badges" });
+        this.setState({ loading: false, error: "Error loading badges :d" });
       });
   }
 
   validateJson(json) {
+    console.log(json);
     if (json.hasOwnProperty("data")) {
-      const badges = this.filterByDate(json.data);
+      //const badges = this.filterByDate(json.data);
       this.setState({ badges: badges });
     } else {
-      this.setState({ error: json.data.error, loading: false });
+      this.setState({ error: json.data.error });
     }
   }
 
@@ -51,12 +57,25 @@ export default class LoggedIn extends Component {
   }
 
   render() {
+    const { error } = this.state;
+    const { errorTextStyle } = styles;
+
     return (
       <View>
         <Text>SEIUM</Text>
         <Text>{this.state.badges}</Text>
+        <Text style={errorTextStyle}>{error}</Text>
         <Button onPress={this.props.deleteJWT} title="Log Out" />
       </View>
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    alignSelf: "center",
+    borderBottomWidth: 1,
+    fontSize: 18,
+    color: "red"
+  }
+};
