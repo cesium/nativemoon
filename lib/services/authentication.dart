f078a675749fa6994bfd1ca4c37652c6f27c9daf
BrunoMartins11 <brunocmartins@live.com.pt>
2019-08-13 09:53:55 +0100

    Adds popUp to inform invalid credencials

--- lib/services/authentication.dart
+++ lib/services/authentication.dart
@@ -3,24 +3,23 @@ import 'dart:async';
 import 'package:http/http.dart' as http;
 
 class Authentication {
-
   final String token;
   final bool valid;
 
   Authentication({this.token, this.valid});
 
   factory Authentication.fromJson(Map<String, dynamic> json) {
-    return Authentication(token : json['jwt'], valid: true);
+    return Authentication(token: json['jwt'], valid: true);
   }
 }
 
-Future<Authentication> fetchAuthToken(String email, String password) async{
-
+Future<Authentication> fetchAuthToken(String email, String password) async {
+  
   final response = await http.post('https://safira20.herokuapp.com/api/auth/sign_in?email=' + email + '&password=' + password);
 
   if (response.statusCode == 200) {
-      return Authentication.fromJson(json.decode(response.body));
+    return Authentication.fromJson(json.decode(response.body));
   } else {
-      return new Authentication(token: '', valid: false);
+    return new Authentication(token: '', valid: false);
   }
 }
