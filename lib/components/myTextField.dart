e9ec60273a779f03707c880e83196ccaffaae89b
BrunoMartins11 <brunocmartins@live.com.pt>
2019-08-13 12:13:35 +0100

    Changes MyTextField to better implementation

--- lib/components/myTextField.dart
+++ lib/components/myTextField.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
-class MyTextField extends StatelessWidget{
-  
+class MyTextField extends StatelessWidget {
   final String hintText;
   final bool filled;
   final Color filledColor;
@@ -10,47 +9,24 @@ class MyTextField extends StatelessWidget{
   final bool hideText;
   String value;
 
-  MyTextField(
-    this.hintText,
-    this.filled,
-    this.filledColor,
-    this.textColor,
-    this.border,
-    this.hideText
-  );
+  MyTextField(this.hintText, this.filled, this.filledColor, this.textColor,
+      this.border, this.hideText);
 
-@override
-  Widget build(BuildContext context){
-    if(hideText){
-        return TextField(
-              onChanged: (text) {
-                this.value = text;
-              },
-              obscureText: hideText,
-              decoration: InputDecoration(
-                  filled: filled,
-                  fillColor: filledColor,
-                  contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-                  hintText: hintText,
-                  border:
-                      OutlineInputBorder(borderRadius: BorderRadius.circular(border))),
-            );
-    } else {
-        return TextField(
-                onChanged: (text) {
-                  this.value = text;
-              },
-                obscureText: hideText,
-                decoration: InputDecoration(
-                    filled: filled,
-                    fillColor: filledColor,
-                    contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-                    hintText: hintText,
-                    labelStyle: new TextStyle(color: textColor),
-                    border:
-                        OutlineInputBorder(borderRadius: BorderRadius.circular(border))),
-              );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onChanged: (text) {
+        this.value = text;
+      },
+      obscureText: hideText,
+      decoration: InputDecoration(
+          filled: filled,
+          fillColor: filledColor,
+          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: hintText,
+          labelStyle: !hideText ? new TextStyle(color: textColor) : null,
+          border:
+              OutlineInputBorder(borderRadius: BorderRadius.circular(border))),
+    );
   }
-
 }
