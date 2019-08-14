68dcefbf0941b7e2ef8a12513087117a9f78cb5a
BrunoMartins11 <brunocmartins@live.com.pt>
2019-08-14 14:21:15 +0100

    Places .env in the correct dir

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:nativemoon/routes.dart';
 
 void main() async {
-  await DotEnv().load('lib/.env');
+  await DotEnv().load('.env');
   new Routes();
   
 }
