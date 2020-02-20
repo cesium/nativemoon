22d9e048e90164e81d646d01e631f61747b610e8
Henrique Pereira <pereirapalmeirahenrique@gmail.com>
2020-02-20 01:40:06 +0000

    back handler fixed login

--- lib/screens/Login/index.dart
+++ lib/screens/Login/index.dart
@@ -71,35 +71,38 @@ class _LoginPageState extends State<LoginPage> {
         : new RoundedButton(
             "Login", Colors.orange[200], 5.0, 0, 0, 30.0, _auth);
 
-    return Scaffold(
-      body: Center(
-        child: Container(
-          color: Colors.cyan[900],
-          child: Padding(
-            padding: const EdgeInsets.all(36.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(
-                  height: 55.0,
-                  child: Image.asset(
-                    "assets/logo_enei.png",
-                    fit: BoxFit.contain,
+    return new WillPopScope(
+      onWillPop: () async => false,
+      child: new Scaffold(
+        body: Center(
+          child: Container(
+            color: Colors.cyan[900],
+            child: Padding(
+              padding: const EdgeInsets.all(36.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(
+                    height: 55.0,
+                    child: Image.asset(
+                      "assets/logo_enei.png",
+                      fit: BoxFit.contain,
+                    ),
                   ),
-                ),
-                SizedBox(height: 20.0),
-                emailField,
-                SizedBox(height: 20.0),
-                passwordField,
-                SizedBox(
-                  height: 15.0,
-                ),
-                loginButton,
-                SizedBox(
-                  height: 15.0,
-                ),
-              ],
+                  SizedBox(height: 20.0),
+                  emailField,
+                  SizedBox(height: 20.0),
+                  passwordField,
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  loginButton,
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
