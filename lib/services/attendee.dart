2b663e731d9c2374bbc4b83762144598ccc0bfe6
brunomartins11 <brunomartins@live.com.pt>
2020-02-23 11:25:39 +0000

    stuff

--- lib/services/attendee.dart
+++ lib/services/attendee.dart
@@ -46,7 +46,7 @@ Future<Attendee> fetchAttendee(String token, String id) async {
 Future<List<Badge>> fetchAttendeeBadges(String token, String id) async {
   print(token);
   final response = await http.get(
-    DotEnv().env['API_URL'] + '/api/v1/attendees/' + id,
+    DotEnv().env['API_URL'] + 'api/v1/attendees/' + id,
     headers: {"Authorization": "Bearer " + token},
   );
 
